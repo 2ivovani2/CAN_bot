521807837bc32140e5bd24a70add0550858fe630
fathutnik <40601479+fathutnik@users.noreply.github.com>
2022-02-16 11:49:19 +0300

    Update urls.py

--- can_django_bot/can_django_bot/urls.py
+++ can_django_bot/can_django_bot/urls.py
@@ -5,7 +5,7 @@ from bot.views import *
 from django.conf import settings
 
 urlpatterns = [
-    path('admin/', admin.site.urls, {'document_root': settings.STATIC_ROOT}),
+    path('admin/', admin.site.urls),
     path('', index),
     path('parse_wb_product', parse_wb_data)
 ]
